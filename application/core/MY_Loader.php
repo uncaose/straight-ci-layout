71f5d2ba35596669129888cb9a907cc9c62ae708
C.H <uncaose@gmail.com>
2017-03-20 15:03:16 +0900

    Update MY_Loader.php

--- application/core/MY_Loader.php
+++ application/core/MY_Loader.php
@@ -6,60 +6,49 @@
 class MY_Loader extends CI_Loader
 {
 	public $_ci_view_path = '';     // ci3
-	public $_views = Array();		// view lists
-	public $_skin = '_skin';		// skin
+	public $_views = Array();	// view lists
+	public $_skin = '_skin';	// skin
 	public $_layout = '_layout';	// layout
-	public function __construct()
-	{
+	
+	public function __construct() {
 		parent::__construct();
-		// ci2 path setting
-		if( empty($this->_ci_view_path) && ! empty($this->_ci_view_paths) )
-		{
+		if( empty($this->_ci_view_path) && ! empty($this->_ci_view_paths) ) { // ci2 path setting
 			$this->_ci_view_path = array_keys($this->_ci_view_paths)[0];
 		}
 	}
 	
 	// Extend
-	public function view( $view, $vars = array(), $return = FALSE, $unshift = FALSE )
-	{
-		if( $unshift )
-		{
-			array_unshift( $this->_views, substr($view, strpos($view, '/')===0?1:0) ); // 뷰 로드 저장
-		}
-		else
-		{
-			array_push( $this->_views, substr($view, strpos($view, '/')===0?1:0) ); // 뷰 로드 저장
+	public function view( $view, $vars = array(), $return = FALSE, $unshift = FALSE ) {
+		if( $unshift ) {
+			array_unshift( $this->_views, substr($view, strpos($view, '/')===0?1:0) ); // store viewname
+		} else {
+			array_push( $this->_views, substr($view, strpos($view, '/')===0?1:0) ); // store viewname
 		}
 		return parent::view($view, $vars, $return);
 	}
 	
-	public function getView( $unique=FALSE )
-	{
-		if( $unique === TRUE )
-		{
+	public function getView( $unique=FALSE ) {
+		if( $unique === TRUE ) {
 			return array_unique( $this->_views );
 		}
 		return $this->_views;
 	}
 	
-	public function setSkin( $name='' )
-	{
+	public function setSkin( $name='' ) {
 		$this->_skin = $name;
 		return $this;
 	}
 	
-	public function getSkin()
-	{
+	public function getSkin() {
 		return $this->_skin;
 	}
-	public function setLayout( $name='' )
-	{
+
+	public function setLayout( $name='' ) {
 		$this->_layout = $name;
 		return $this;
 	}
 	
-	public function getLayout()
-	{
+	public function getLayout() {
 		return $this->_layout;
 	}
 }
