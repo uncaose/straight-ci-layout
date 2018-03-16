76af2f30db1714b2699b7a9d77c812c9f85b2bc8
C.H <uncaose@gmail.com>
2018-03-16 18:58:41 +0900

    CI2 Class Ucfirst Asset.php

---
+++ application/controllers/asset.php
@@ -0,0 +1,21 @@
+<?php if (!defined('BASEPATH')) exit('No direct script access allowed');
+
+/**
+ * @author	: uncaose@gmail.com
+ * @url 	: https://github.com/uncaose/straight-ci-layout
+ */
+class Asset extends CI_Controller
+{
+	public function __construct() {
+		parent::__construct();
+	}
+
+	public function _remap( $method, $params = array() ) {
+		$this->load->driver('straight');
+		echo $this->straight->layout->asset( VIEWPATH.join('/', $params) );
+	}
+}
+
+/**
+ * End of File application/controllers/Asset.php
+ */
