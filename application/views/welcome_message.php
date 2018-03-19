d031efca9fa3a1294b8740f4a9358c50e6f189b4
C.H <uncaose@gmail.com>
2018-03-19 10:53:17 +0900

    welcome_mesage.php update

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -4,7 +4,11 @@
 		<p>The page you are looking at is being generated dynamically by CodeIgniter.</p>
 
 		<p>If you would like to edit this page you'll find it located at:</p>
-		<code>application/views/welcome_message.php</code>
+		<code>
+        application/views/welcome_message.css<br/>
+        application/views/welcome_message.js<br/>
+        application/views/welcome_message.php
+        </code>
 
 		<p>The corresponding controller for this page is found at:</p>
 		<code>application/controllers/Welcome.php</code>
