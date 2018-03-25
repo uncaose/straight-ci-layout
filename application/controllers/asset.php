dfa40efa20e24d85b4a27367fdb4c99b2eee9bb3
C.H <uncaose@gmail.com>
2018-03-25 17:46:52 +0900

    asset.php update

--- application/controllers/asset.php
+++ application/controllers/asset.php
@@ -1,5 +1,7 @@
 <?php if (!defined('BASEPATH')) exit('No direct script access allowed');
 
+use MatthiasMullie\Minify;
+
 /**
  * @author	: uncaose@gmail.com
  * @url 	: https://github.com/uncaose/straight-ci-layout
@@ -11,11 +13,27 @@ class Asset extends CI_Controller
 	}
 
 	public function _remap( $method, $params = array() ) {
+
+		$file = VIEWPATH.join('/', $params);
+		
 		$this->load->driver('straight');
-		echo $this->straight->layout->asset( VIEWPATH.join('/', $params) );
+		$this->straight->layout->header( $file );
+
+		if( class_exists('MatthiasMullie\\Minify\\JS') )
+		{
+			if( $method == 'js' ){
+				$minifier = new Minify\JS( $file );
+			}else{
+				$minifier = new Minify\CSS( $file );
+			}
+			echo $minifier->minify();
+		}else{
+			$this->straight->layout->asset( $file );
+		}
+
 	}
 }
 
 /**
- * End of File application/controllers/Asset.php
+ * End of File application/controllers/asset.php
  */
